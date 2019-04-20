c24d0ce9eb210ada26349d8fdf10725f7a262f3d
TanzimAhmed <47281426+TanzimAhmed@users.noreply.github.com>
2019-04-20 21:49:48 +0600

    Admin Products view updated
    Forms added for
    - adding products
    - adding category
    - deleting category
    - updating image

--- ProjectCodes/orange/admin.py
+++ ProjectCodes/orange/admin.py
@@ -11,17 +11,19 @@ def admin_perform_query(action, new_value, target_category_id, target_product_id
         product.update_category('categoryID', new_value['category_id'], target_category_id)
     elif action == "update_category_name":
         product.update_category('categoryName', new_value['category_name'], target_category_id)
-    elif action == "update product_id":
+    elif action == "update_product_id":
         product.update_product_details('productID', new_value['product_id'], target_category_id, target_product_id)
-    elif action == "update product_name":
+    elif action == "update_product_name":
         product.update_product_details('name', new_value['name'], target_category_id, target_product_id)
-    elif action == "update product_price":
+    elif action == "update_product_price":
         product.update_product_details('price', new_value['price'], target_category_id, target_product_id)
-    elif action == "update product_cost":
+    elif action == "update_product_cost":
         product.update_product_details('cost', new_value['cost'], target_category_id, target_product_id)
-    elif action == "update product_quantity":
+    elif action == "update_product_quantity":
         product.update_product_details('quantityLeft', new_value['quantity_left'],
                                        target_category_id, target_product_id)
+    elif action == 'add_category':
+        product.add_category(new_value['category_id'], new_value['category_name'])
     elif action == 'add_product':
         admin_add_product(new_value, picture, UPLOAD_FOLDER, ALLOWED_EXTENSIONS)
 
